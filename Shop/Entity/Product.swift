26e0cc762cce8aa29be6959158f2e34c1bd42425
yhkaplan <yhkaplan@gmail.com>
2020-07-02 11:23:48 +0900

    Make Product parsing from JSON work

--- Shop/Entity/Product.swift
+++ Shop/Entity/Product.swift
@@ -8,10 +8,33 @@
 
 import Foundation
 
-struct Product: Hashable, Decodable {
-    let id: String
+struct Product: Hashable {
+    let id: String // TODO: make ID type-safe https://www.swiftbysundell.com/articles/type-safe-identifiers-in-swift/
     let name: String
-    let price: String // TODO: Make into Int and decode
-    // TODO: imageURL not parsing
-    let imageURL: String = "" // This would normally be a URL, but is instead a string to represent an SFSymbol
+    let price: Double
+    let imageURL: String // This would normally be a URL, but is instead a string to represent an SFSymbol
+
+    var formattedPrice: String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencyCode = "USD"
+        return formatter.string(from: .init(value: price))
+    }
+}
+
+extension Product: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case id, name, price
+        case imageURL = "image_url"
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let priceString = try container.decode(String.self, forKey: .price)
+        guard let price = Double(priceString) else { throw DecoderError.cannotParse }
+        self.price = price
+
+        id = try container.decode(String.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+        imageURL = try container.decode(String.self, forKey: .imageURL)
+    }
 }
