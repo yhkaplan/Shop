3d48bc94dd224449485106c8cf8b50513967208f
yhkaplan <yhkaplan@gmail.com>
2020-07-09 18:20:45 +0900

    Make ProductDetailView

---
+++ Shop/Scenes/Home/View/ProductDetailView.swift
@@ -0,0 +1,9 @@
+//
+//  HomeDetailView.swift
+//  Shop
+//
+//  Created by josh on 2020/07/09.
+//  Copyright © 2020 yhkaplan. All rights reserved.
+//
+
+import Foundation
---
+++ Shop/Scenes/Product/ProductDetailView.swift
@@ -0,0 +1,40 @@
+//
+//  ProductDetailView.swift
+//  Shop
+//
+//  Created by josh on 2020/07/09.
+//  Copyright © 2020 yhkaplan. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProductDetailView: View {
+    let product: Product
+
+    var body: some View {
+        VStack {
+            Image(systemName: product.imageURL)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            Text("\(product.formattedPrice ?? "-")")
+            Spacer()
+        }.navigationBarTitle(product.name)
+    }
+}
+#if DEBUG
+extension Product {
+    static var previewData: Self {
+        .init(id: "123", name: "Banana", price: 7.0, imageURL: "globe")
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ProductDetailView(product: Product.previewData)
+        }
+    }
+}
+#endif
